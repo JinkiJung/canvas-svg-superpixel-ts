422acbf5f75287f199b175cd7c51303eb9b7281c
Jinki Jung <your.jinki.jung@gmail.com>
2020-11-18 14:48:27 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Annotation } from './superpixel';
+import data from "./test.jpg.json";
 import './App.css';
+import './imageOverlay.css';
+import { CanvasSuperpixel } from './canvasSuperpixel';
+const React = require('react');
+
+
+const colors = ["remove", "#5db300", 
+"#e81123", 
+"#6917aa", 
+"#015cda",
+"#4894fe", 
+"#6b849c",
+"#70c400", 
+"#f7929a", 
+"#257ffe", ];
+
+const annotatedList: Annotation[] = [ new Annotation(1,colors[1], 1), new Annotation(2,colors[2], 2) ];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CanvasSuperpixel keyId={"mainCanvas"} fileName={"./resource/test.jpg"} segmentationData={data} annotationData={annotatedList} width={1024} height={768} defaultcolor={"black"} colorbuttons={colors}/> 
     </div>
   );
 }
